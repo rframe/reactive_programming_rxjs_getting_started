7dac52492d89cd937c1981050ff05a810657211b
russell f <russell.frame@machiningcloud.com>
2018-02-26 08:36:18 -0800

    Working with Observables: Sending Requests with XmlHttpRequest

--- main.ts
+++ main.ts
@@ -4,17 +4,29 @@ export const logValue = (value) => console.log(value);
 export const logError = (e) => console.log(`error ${e}`);
 export const logComplete = () => console.log('complete');
 
-let circle = document.getElementById('circle');
-let source = Observable.fromEvent(document, "mousemove")
-        .map((event: MouseEvent) => ({x: event.clientX, y: event.clientY}))
-        .filter((value) => value.x < 500)
-        .delay(300);
+let output = document.getElementById('output');
+let button = document.getElementById('button');
 
-function onNext(value) {
-    circle.style.left = `${value.x}px`;
-    circle.style.top = `${value.y}px`;
+let click = Observable.fromEvent(button, 'click');
+
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m => {
+            let div = document.createElement('div');
+            div.innerText = m.title;
+            output.appendChild(div);
+        })
+    })
+    xhr.open('GET', url);
+    xhr.send();
 }
 
-source.subscribe(onNext, logError, logComplete);
+click.subscribe(
+        e => load('movies.json'),
+        logError,
+        logComplete);
 
 
