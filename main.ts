773db2dcc3e4d0bf40d839c6b14633f0bcabeaf8
russell f <russell.frame@machiningcloud.com>
2018-02-26 08:06:15 -0800

    Observers and Observables: An Easier Observer

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 import {Observable, Observer} from 'rxjs';
 
+export const logValue = (value) => console.log(value);
+export const logError = (e) => console.log(`error ${e}`);
+export const logComplete = () => console.log('complete');
+
 let numbers = [1, 5, 10];
 let source = Observable.from(numbers);
 
+
+source.subscribe(logValue, logError, logComplete);
+
 class MyObserver implements Observer<number> {
     next(value) {
-        console.log(`value: ${value}`);
+        logValue(value);
     }
     error(e) {
-        console.log(`error ${e}`);
+        logError(e);
     }
     complete() {
-        console.log('complete');
+        logComplete();
     }
 }
 
-source.subscribe(new MyObserver());
-source.subscribe(new MyObserver());
+// source.subscribe(new MyObserver());
+// source.subscribe(new MyObserver());
 
