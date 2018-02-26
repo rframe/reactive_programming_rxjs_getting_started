1b0f3250354dfd266e7c5728923779a21d4787e7
russell f <russell.frame@machiningcloud.com>
2018-02-26 08:09:40 -0800

    Observers and Observables: Using Observable.create

--- main.ts
+++ main.ts
@@ -1,27 +1,21 @@
-import {Observable, Observer} from 'rxjs';
+import {Observable} from 'rxjs';
 
 export const logValue = (value) => console.log(value);
 export const logError = (e) => console.log(`error ${e}`);
 export const logComplete = () => console.log('complete');
 
 let numbers = [1, 5, 10];
-let source = Observable.from(numbers);
+let source = Observable.create((observer) => {
+    for(let n of numbers) {
+        // if(n === 5) {
+        //     observer.error('Something went wrong!');
+        // }
+        observer.next(n);
+    }
+    observer.complete();
+});
 
 
 source.subscribe(logValue, logError, logComplete);
 
-class MyObserver implements Observer<number> {
-    next(value) {
-        logValue(value);
-    }
-    error(e) {
-        logError(e);
-    }
-    complete() {
-        logComplete();
-    }
-}
-
-// source.subscribe(new MyObserver());
-// source.subscribe(new MyObserver());
 
